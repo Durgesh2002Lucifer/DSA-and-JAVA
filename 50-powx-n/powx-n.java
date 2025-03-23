57acad8f651859eb51a2c12eaa1ee0bbae659037
Durgesh <161637882+Durgesh2002Lucifer@users.noreply.github.com>
2025-03-23 22:28:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (74.35%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,21 @@
+class Solution {
+    /*double solve(double x, long n){
+        if(n==0) return 1;
+
+       if(n==1) return x; 
+       if(n<0){
+        return Math.pow(1/x,-n);
+       }
+      double half = Math.pow(x,n/2);
+      if(n%2 == 0){
+        return half*half;
+      }else{
+        return half*half*x;
+      }
+    }  */
+    public double myPow(double x, int n) {
+       
+     //  return solve(x,(long)n);
+     return Math.pow(x,n);
+    }
+}
