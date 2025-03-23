c06c72879e50f8575db28212a83f3a5981cc3bbd
Durgesh <161637882+Durgesh2002Lucifer@users.noreply.github.com>
2025-03-23 22:39:19 +0530

    Time: 1 ms (86.13%) | Memory: 41 MB (52.49%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,26 +1,24 @@
 class Solution {
     public int mySqrt(int x) {
 
-
-       if (x == 0 || x == 1) return x; // Base cases
+        if (x == 0 || x == 1) return x;
         
         int low = 1, high = x, ans = 0;
         
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            long square = (long) mid * mid; // Avoid overflow
+            long square = (long) mid * mid; 
             
             if (square == x)
-                return mid; // Perfect square found
+                return mid; 
             else if (square < x) {
                 low = mid + 1;
-                ans = mid; // Store possible answer
+                ans = mid; 
             } else {
                 high = mid - 1;
             }
-        }
-        
-        return ans; // Return the integer square root
+        }        
+        return ans; 
     }
 }
    
