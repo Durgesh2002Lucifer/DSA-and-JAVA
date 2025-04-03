45697f7ddc1bd2097d7e2812ba68da5f90d76ab7
Durgesh <161637882+Durgesh2002Lucifer@users.noreply.github.com>
2025-04-03 20:59:42 +0530

    Time: 1 ms (84.72%) | Memory: 42.2 MB (24.92%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -14,15 +14,20 @@ class Solution {
         }
 
         for(int i=1;i<numRows;i++){
+            // result ke previous element ko (i-1) ko prevRows mai rakh denge
             List<Integer> prevRows = result.get(i-1);
-
+            // row ki starting mai 1 add karenge
             List<Integer> row = new ArrayList<>();
             row.add(1);
             for(int j=0;j<i-1;j++){
+                //middle element ke uppar ke doo j and j+1 element ka sum
                 row.add(prevRows.get(j) + prevRows.get(j+1));
             }
+            // row ki end mai phir 1 add karenge
             row.add(1);
+            // or last mai puri row ko result mai print kar denge
             result.add(row);
+            // or yai silsila uh hi chalta jayga jab loop break na hoo.
         }
         return result;
     }
