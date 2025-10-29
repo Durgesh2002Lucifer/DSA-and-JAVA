ee343b096e8564989e6936f3e4e3bdff11d15d7b
Durgesh <161637882+Durgesh2002Lucifer@users.noreply.github.com>
2025-10-29 22:45:07 +0530

    Time: 0 ms (100.00%) | Memory: 57.4 MB (54.31%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -2,18 +2,18 @@ class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
         k = k%n;
-        int ans[] = new int[n];
-        int j =0;
+        reverse(nums,0,n-k-1);
+        reverse(nums,n-k,n-1);
+        reverse(nums,0,n-1);
+    }
+    public void reverse(int arr[], int i, int j){
         
-        for(int i=n-k;i<n;i++){
-            ans[j++] = nums[i];
-        }
-        for(int i=0;i<n-k;i++){
-            ans[j++] = nums[i];
-        }
-        // main array mai answer array ko copy karna ha bass 
-        for(int i=0;i<ans.length;i++){
-            nums[i] = ans[i];
+        while(i < j){
+           int temp = arr[i];
+           arr[i] = arr[j];
+           arr[j] = temp;
+           i++;
+           j--;
         }
     }
 }
